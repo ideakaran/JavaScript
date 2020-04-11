7bb6cfba9dbe4a847bdcc156a24d72403dbd2fc0
Karan Pant <karan.pant@ambarkaar.com>
2020-04-11 16:29:21 +0545

    Served file from spike

---
+++ spikes/node_http_servefile/http_servefile.js
@@ -0,0 +1,14 @@
+"use strict";
+var http = require('http');
+var server = http.createServer();
+var fs =require("fs");
+server.on("request", function(request, response) {
+    console.log("Received request");
+    fs.readFile("file.html", function(err, data) {
+        if(err) throw err;
+        response.end(data);
+    });
+
+});
+server.listen(8080);
+console.log("Server started");
