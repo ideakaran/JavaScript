1b6ead3c565518739d6e33df4ccc9d4c0a42c175
Karan Pant <karan.pant@ambarkaar.com>
2020-04-22 10:32:08 +0545

    Pulled DOM element from testacular

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -7,8 +7,12 @@
         it('should run', function () {
             var div = document.createElement("div");
             div.setAttribute("id", "tdjs");
+            div.setAttribute("foo", "bar");
             document.body.appendChild(div);
             dump('breakpoint here');
+
+            var extractedDiv = document.getElementById("tdjs");
+            expect(extractedDiv.getAttribute("foo")).to.equal("bar");
             expect("foo").to.equal("foo");
         });
     });
