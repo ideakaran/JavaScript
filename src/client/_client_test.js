8ef8292c4f089bd8ff40433446291e3e2180a8f9
Karan Pant <karan.pant@ambarkaar.com>
2020-04-26 10:46:39 +0545

    Clean up DOM after each test

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,9 +1,14 @@
-/*global describe, it, expect, dump, $, wwp*/
+/*global describe, it, expect, dump, afterEach, $, wwp*/
 
 (function() {
     "use strict";
     //var assert = chai.assert;
     describe("Drawing area", function() {
+
+        afterEach(function() {
+            $("#wwp-drawingArea").remove();
+        });
+
         it('should be initialized in predefined div', function () {
             //create div that's assumed to be in our home page
             var div = document.createElement("div");
@@ -24,13 +29,24 @@
                 expect(tagName).to.equal("div");
             }
 
-
             /*jshint -W087 */
             //debugger;
         });
 
         it("should have the same dimensions as its enclosing div", function(){
-            //TODO
-        })
+            var div = $("#wwp-drawingArea");
+            expect(div.length).to.equal(0);
+            var testHTML = "<div style='height: 200; width: 400; border: solid red 2px'>hi</div>";
+            $(document.body).append(testHTML);
+            dump("hi");
+            // var div = document.createElement("div");
+            // div.setAttribute("id", "wwp-drawingArea");
+            // document.body.appendChild(div);
+            //
+            // wwp.initializeDrawingArea("wwp-drawingArea");
+            // var tagName = $(div).children()[0].tagName;
+
+
+        });
     });
 })();
