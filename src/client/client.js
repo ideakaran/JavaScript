08a68612d16da1e338b9345c37de0c50d73f7ea4
Karan Pant <karan.pant@ambarkaar.com>
2020-04-27 22:00:07 +0545

    Spiked drawing a line with Raphael

--- src/client/client.js
+++ src/client/client.js
@@ -6,8 +6,9 @@ var wwp = {};
     "use strict";
 
     wwp.initializeDrawingArea = function(drawingAreaElement) {
-        dump(Raphael.type);
+        //dump(Raphael.type);
         var paper = new Raphael(drawingAreaElement);
+        paper.path("M20, 30L200, 20");
         return paper;
     };
 
